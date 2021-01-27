92254aad5cd1baadb8e6cba57a6fe5a8d72e32c1
Agustin Amato <agustin.amato96@gmail.com>
2021-01-27 11:22:41 -0300

    creo snmp service

---
+++ app/Http/Services/SnmpService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Services;
+
+use Ndum\Laravel\Facades\Snmp;
+
+class SnmpService
+{
+    private $_snmp;
+
+    public function __construct() {
+        $this->_snmp = Snmp::newClient('127.0.0.1', 1, 'public');
+    }
+
+    public function getRequest() {
+        $process = $this->_snmp->getValue('1.3.6.1.2.1.25.1.6.0');  //Procesos
+
+        $session = $this->_snmp->getValue('1.3.6.1.2.1.25.1.5.0');  //Sesiones
+
+        return [
+            'process' => $process,
+            'session' => $session,
+        ]
+    }
+}
